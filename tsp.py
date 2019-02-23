9379177a6bef64e7843e3795bacc5a6d695c48b6
Vishnu Chakroborty <42984263+vishnuchakr@users.noreply.github.com>
2019-02-23 17:32:28 -0600

    Created the distance_optimizer method in tsp.py

--- tsp.py
+++ tsp.py
@@ -1,2 +1,34 @@
+from ortools.constraint_solver import pywrapcp
+from ortools.constraint_solver import routing_enums_pb2
+from routeOptimizer.py import get_distance
 
+# Distance callback
+def create_distance_callback(dist_matrix):
+  # Create a callback to calculate distances between cities.
 
+  def distance_callback(from_node, to_node):
+      return int(dist_matrix[from_node][to_node])
+
+  return distance_callback
+
+def optimal_route():
+    
+    distance_array = get_distance('CSV/PATIENT_TASK_DATA.csv', 55)
+    '''
+    tsp_size = len(distance_array)
+    num_routes = 1
+    depot = 0
+        
+    # Create routing model
+    if tsp_size > 0:
+        routing = pywrapcp.RoutingModel(tsp_size, num_routes, depot)
+        search_parameters = pywrapcp.RoutingModel.DefaultSearchParameters()
+
+    # Create the distance callback.
+        dist_callback = create_distance_callback(dist_matrix)
+        routing.SetArcCostEvaluatorOfAllVehicles(dist_callback)
+
+    # Solve the problem.
+        assignment = routing.SolveWithParameters(search_parameters)
+    '''
+    print(distance_array)
