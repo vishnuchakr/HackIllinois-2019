6c0932cf259b48d1c674ba6bfa8a3c1eae2d0462
Fgoyal <fgoyal2@illinois.edu>
2019-02-23 19:40:21 -0600

    Wrote stuff with GoogleMaps?

--- Optumization/Optumization/DailyMapViewController.swift
+++ Optumization/Optumization/DailyMapViewController.swift
@@ -8,26 +8,42 @@
 
 import UIKit
 import GoogleMaps
+import GooglePlaces
 
 class DailyMapViewController: UIViewController {
     
-    @IBOutlet fileprivate weak var mapView: GMSMapView!
+    @IBOutlet var mapView: GMSMapView!
+    var marker = GMSMarker()
+    var sourceLat = 23.0225
+    var sourceLong = 72.5714
+    var rectangle = GMSPolyline()
+    //var snackbar: MJSnackBar!
     
+    // You don't need to modify the default init(nibName:bundle:) method.
     override func viewDidLoad() {
         super.viewDidLoad()
-        let camera = GMSCameraPosition.camera(withLatitude: 37.36, longitude: -122.0, zoom: 6.0)
-        mapView.camera = camera
-        showMarker(position: camera.target)
     }
     
-    func showMarker(position: CLLocationCoordinate2D){
-        let marker = GMSMarker()
-        marker.position = position
-        marker.title = "Palo Alto"
-        marker.snippet = "San Francisco"
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: sourceLat, longitude: sourceLong, zoom: 12.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let position = CLLocationCoordinate2DMake(sourceLat, sourceLong)
+        self.marker = GMSMarker(position: position)
+        let pinColor = UIColor.orange
+        self.marker.icon = GMSMarker.markerImage(with: pinColor)
+        self.marker.map = self.mapView
+        
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
         marker.map = mapView
+        
+        //self.snackbar = MJSnackBar(onView: self.view)
+        
+        
     }
-}
-extension ViewController: GMSMapViewDelegate{
-    
 }
