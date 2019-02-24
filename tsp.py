c346a9c9a2ec3cc0303df6ece93b3f2d83863786
Vishnu Chakroborty <42984263+vishnuchakr@users.noreply.github.com>
2019-02-23 18:18:13 -0600

    Add files via upload

--- tsp.py
+++ tsp.py
@@ -1,6 +1,6 @@
 from ortools.constraint_solver import pywrapcp
 from ortools.constraint_solver import routing_enums_pb2
-from routeOptimizer.py import get_distance_matrix
+import routeOptimizer.py
 
 # Distance callback
 def create_distance_callback(dist_matrix):
@@ -13,7 +13,7 @@ def create_distance_callback(dist_matrix):
 
 def optimal_route():
     
-    distance_array = get_distance_matrix('CSV/PATIENT_TASK_DATA.csv', 55)
+    distance_array = routeOptimizer.get_distance_matrix('PATIENT_TASK_DATA.csv', 55)
     
     tsp_size = len(distance_array)
     num_routes = 1
@@ -24,31 +24,31 @@ def optimal_route():
         routing = pywrapcp.RoutingModel(tsp_size, num_routes, depot)
         search_parameters = pywrapcp.RoutingModel.DefaultSearchParameters()
 
-    # Create the distance callback.
-    dist_callback = create_distance_callback(dist_matrix)
-    routing.SetArcCostEvaluatorOfAllVehicles(dist_callback)
+        # Create the distance callback.
+        dist_callback = create_distance_callback(dist_matrix)
+        routing.SetArcCostEvaluatorOfAllVehicles(dist_callback)
 
-    # Solve the problem.
-    assignment = routing.SolveWithParameters(search_parameters)
+        # Solve the problem.
+        assignment = routing.SolveWithParameters(search_parameters)
 
-    if assignment:
-        # Solution distance.
-        print "Total distance: " + str(assignment.ObjectiveValue()) + " miles\n"
-        # Display the solution.
-        # Only one route here; otherwise iterate from 0 to routing.vehicles() - 1
-        route_number = 0
-        index = routing.Start(route_number) # Index of the variable for the starting node.
-        route = ''
-        while not routing.IsEnd(index):
-            # Convert variable indices to node indices in the displayed route.
-            route += str(city_names[routing.IndexToNode(index)]) + ' -> '
-            index = assignment.Value(routing.NextVar(index))
-        route += str(city_names[routing.IndexToNode(index)])
-        print "Route:\n\n" + route
+        if assignment:
+            # Solution distance.
+            print("Total distance: " + str(assignment.ObjectiveValue()) + " miles\n")
+            # Display the solution.
+            # Only one route here; otherwise iterate from 0 to routing.vehicles() - 1
+            route_number = 0
+            index = routing.Start(route_number) # Index of the variable for the starting node.
+            route = ''
+            while not routing.IsEnd(index):
+                # Convert variable indices to node indices in the displayed route.
+                route += str(city_names[routing.IndexToNode(index)]) + ' -> '
+                index = assignment.Value(routing.NextVar(index))
+            route += str(city_names[routing.IndexToNode(index)])
+            print("Route:\n\n" + route)
+        else:
+            print('No solution found.')
+
     else:
-      print 'No solution found.'
-
-    else:
-        print 'Specify an instance greater than 0.'
+        print('Specify an instance greater than 0.')
     
     print(distance_array)
