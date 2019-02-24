f0e06fa8c8ac6af75918991004012b14ad7f2da5
Fgoyal <fgoyal2@illinois.edu>
2019-02-23 20:39:00 -0600

    updates

---
+++ Optumization/APIKEY.swift
@@ -0,0 +1,13 @@
+//
+//  APIKEY.swift
+//  Optumization
+//
+//  Created by Fiza Goyal on 2/23/19.
+//  Copyright © 2019 HackIllinois. All rights reserved.
+//
+
+import Foundation
+
+public class APIKEY {
+    public static var APIKEY = ""
+}
