518cb8a567421b20f69c8eb33e736c83015b8e29
jmoh3 <jmoh3@illinois.edu>
2019-02-23 20:48:11 -0600

    finished due date matrix

--- backend/get_due_date_matrix.py
+++ backend/get_due_date_matrix.py
@@ -9,4 +9,23 @@ def get_due_date_matrix(filename, employee_id):
 	patient_df = pd.read_csv(filename)
 	patient_df = patient_df[patient_df['employee_id'] == employee_id]
 
-	patient_df['datetime'] = patient_df['due_date'].to_datetime()
+	patient_df['datetime'] = pd.to_datetime(patient_df['due_date'])
+
+	min_date = min(patient_df['datetime'])
+
+	patient_df['time_since_first'] = patient_df['datetime'] - min_date
+
+	patient_df['time_since_first'] = patient_df['time_since_first'].apply(days)
+
+	patient_df['time_since_first'] = patient_df['time_since_first'] + 1
+
+	patient_matrix = np.matrix(patient_df['time_since_first'])
+
+	due_date_matrix = np.matmul(patient_matrix.transpose(), patient_matrix)
+
+	return due_date_matrix
+
+def days(td):
+    return td.days
+
+print(get_due_date_matrix('CSV/PATIENT_TASK_DATA.csv', 1))
