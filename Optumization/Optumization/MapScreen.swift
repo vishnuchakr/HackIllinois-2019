e85c1a5080f8518e642697ce692ec906b1d52181
Fgoyal <fgoyal2@illinois.edu>
2019-02-24 01:03:22 -0600

    Changed colors to match brand

--- Optumization/Optumization/MapScreen.swift
+++ Optumization/Optumization/MapScreen.swift
@@ -1,121 +1,12 @@
-//////
-//////  DailyMapViewController.swift
-//////  Optumization
-//////
-//////  Created by Fiza Goyal on 2/23/19.
-//////  Copyright © 2019 HackIllinois. All rights reserved.
-//////
-////
 //
-//import UIKit
-//import MapKit
-//import CoreLocation
+//  MapScreen.swift
+//  Optumization
 //
-//class MapScreen: UIViewController, CLLocationManagerDelegate {
-//
-//    @IBOutlet weak var mapView: MKMapView!
-//
-//    let locationManager = CLLocationManager()
-//    let regionInMeters: Double = 10000
-//
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        checkLocationServices()
-//
-//
-//    }
-//
-//    func addMarkers(coordinate: CLLocationCoordinate2D, personName: String, address: String) {
-//        let annotation = MKPointAnnotation()
-//        annotation.title = personName
-//        annotation.subtitle = address
-//        annotation.coordinate = coordinate
-//        mapView.addAnnotation(annotation)
-//    }
-//
-//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-//        guard annotation is MKPointAnnotation else { return nil }
-//
-//        let identifier = "Annotation"
-//        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
-//
-//        if annotationView == nil {
-//            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-//            annotationView!.canShowCallout = true
-//        } else {
-//            annotationView!.annotation = annotation
-//        }
-//
-//        return annotationView
-//    }
-//
-//    func setupLocationManager() {
-//        locationManager.delegate = self
-//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-//    }
-//
-//
-//    func centerViewOnUserLocation() {
-//        if let location = locationManager.location?.coordinate {
-//            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-//            mapView.setRegion(region, animated: true)
-//        }
-//    }
-//
-//
-//    func checkLocationServices() {
-//        if CLLocationManager.locationServicesEnabled() {
-//            setupLocationManager()
-//            checkLocationAuthorization()
-//        } else {
-//            // Show alert letting the user know they have to turn this on.
-//        }
-//    }
-//
-//
-//    func checkLocationAuthorization() {
-//        switch CLLocationManager.authorizationStatus() {
-//        case .authorizedWhenInUse:
-//            mapView.showsUserLocation = true
-//            centerViewOnUserLocation()
-//            locationManager.startUpdatingLocation()
-//            break
-//        case .denied:
-//            // Show alert instructing them how to turn on permissions
-//            break
-//        case .notDetermined:
-//            locationManager.requestWhenInUseAuthorization()
-//        case .restricted:
-//            // Show an alert letting them know what's up
-//            break
-//        case .authorizedAlways:
-//            break
-//        }
-//    }
-//
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//        guard let location = locations.last else { return }
-//        let region = MKCoordinateRegion.init(center: location.coordinate, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-//        mapView.setRegion(region, animated: true)
-//    }
-//
-//
-//    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-//        checkLocationAuthorization()
-//    }
-//
-//}
+//  Created by Fiza Goyal on 2/23/19.
+//  Copyright © 2019 HackIllinois. All rights reserved.
 //
 //
 
-//
-//  ViewController.swift
-//  DrawRouteOnMapKit
-//
-//  Created by Aman Aggarwal on 08/03/18.
-//  Copyright © 2018 iostutorialjunction. All rights reserved.
-//
 import UIKit
 import MapKit
 
@@ -140,8 +31,8 @@ class MapScreen: UIViewController, MKMapViewDelegate {
         let sourceLocation = CLLocationCoordinate2D(latitude:40.1138 , longitude: -88.2249)
         let destinationLocation = CLLocationCoordinate2D(latitude:40.1059 , longitude: -88.2273)
         let location2 = CLLocationCoordinate2D(latitude:40.1092 , longitude: -88.2272)
-        createPathBetweenTwoPoints(sourceLocation: sourceLocation, destinationLocation: destinationLocation, sourceLocationName: "", destinationLocationName: "")
-        createPathBetweenTwoPoints(sourceLocation: destinationLocation, destinationLocation: location2, sourceLocationName: "", destinationLocationName: "")
+        createPathBetweenTwoPoints(sourceLocation: sourceLocation, destinationLocation: destinationLocation, sourceLocationName: "this is the title", destinationLocationName: "")
+        createPathBetweenTwoPoints(sourceLocation: destinationLocation, destinationLocation: location2, sourceLocationName: "this is the title", destinationLocationName: "")
         
         //set delegate for mapview
         self.mapView.delegate = self
@@ -185,7 +76,7 @@ class MapScreen: UIViewController, MKMapViewDelegate {
     //MARK:- MapKit delegates
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         let renderer = MKPolylineRenderer(overlay: overlay)
-        renderer.strokeColor = UIColor.blue
+        renderer.strokeColor = UIColor(red:1.00, green:0.38, blue:0.00, alpha:1.0)
         renderer.lineWidth = 4.0
         return renderer
     }
